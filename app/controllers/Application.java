d8c51289aa65c52d1b366e3a2bb614adef80454b
william@windows_machine <bouc@clanz.be>
2016-02-04 15:53:23 +0100

    This totally deserves a commit.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -20,10 +20,31 @@ public class Application extends Controller {
 	
     public Result index() {
         //return ok(index.render("Your new application is ready."));
+    	List<Map<String, String>> articles = new ArrayList<Map<String, String>>();
     	Map<String, String> theMap = new HashMap<String, String>();
-    	theMap.put("SomeKey", "SomeValue");
+    	theMap.put("id", "210");
+    	theMap.put("title", "Some Article");
+    	theMap.put("thumbImage", "images/art.png");
+    	// Should probably be an array:
+    	theMap.put("tags", "tag1, tag2");
+    	theMap.put("commentsCount", "12");
+    	theMap.put("date", new Date().toString());
+    	theMap.put("summary", "This is the summary.");
+    	theMap.put("author", "VeZD");
+    	articles.add(theMap);
+    	theMap = new HashMap<String, String>();
+    	theMap.put("id", "350");
+    	theMap.put("title", "Another Article");
+    	theMap.put("thumbImage", "images/art.png");
+    	// Should probably be an array:
+    	theMap.put("tags", "Technology, Science");
+    	theMap.put("commentsCount", "6");
+    	theMap.put("date", new Date().toString());
+    	theMap.put("summary", "I'm writing a slightly longer summary. Sort of.<br />A line feed too.");
+    	theMap.put("author", "VeZD");
+    	articles.add(theMap);
     	response().setHeader("Access-Control-Allow-Origin", "*");
-    	return ok(Json.toJson(theMap));
+    	return ok(Json.toJson(articles));
     }
 
 }
