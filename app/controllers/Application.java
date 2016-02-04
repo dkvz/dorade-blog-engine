d65174b9ab2ff77ef4db8cbc438495d5c9999579
william@windows_machine <bouc@clanz.be>
2016-02-04 10:00:24 +0100

    With a working modified header for AJAX to work cross-domains.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,10 +9,20 @@ import views.html.*;
 
 public class Application extends Controller {
 
+//	@Before
+//	public static void setCORS() {
+//		Http.Header hd = new Http.Header();
+//		hd.name = "Access-Control-Allow-Origin";
+//		hd.values = new ArrayList<String>();
+//		hd.values.add("http://localhost:9010&quot;);
+//		Http.Response.current().headers.put("Access-Control-Allow-Origin",hd);
+//	}
+	
     public Result index() {
         //return ok(index.render("Your new application is ready."));
     	Map<String, String> theMap = new HashMap<String, String>();
     	theMap.put("SomeKey", "SomeValue");
+    	response().setHeader("Access-Control-Allow-Origin", "*");
     	return ok(Json.toJson(theMap));
     }
 
